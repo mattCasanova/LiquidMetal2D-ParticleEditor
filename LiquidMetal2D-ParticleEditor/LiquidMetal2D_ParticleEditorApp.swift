cdf5275ac2dc651b74455a9fdad46cbd3cd5319a
Matt Casanova <lazersquad@gmail.com>
2026-04-28 21:22:00 -0700

    chore: flatten directory structure

---
+++ LiquidMetal2D-ParticleEditor/LiquidMetal2D_ParticleEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidMetal2D_ParticleEditorApp.swift
+//  LiquidMetal2D-ParticleEditor
+//
+//  Created by Matt Casanova on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct LiquidMetal2D_ParticleEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiquidMetal2D-ParticleEditorTests/LiquidMetal2D_ParticleEditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidMetal2D_ParticleEditorTests.swift
+//  LiquidMetal2D-ParticleEditorTests
+//
+//  Created by Matt Casanova on 4/28/26.
+//
+
+import Testing
+@testable import LiquidMetal2D_ParticleEditor
+
+struct LiquidMetal2D_ParticleEditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
